0366ecd192bfe6a8a855d0dac318ffa533094dc4
motoke215 <motoke0577@gmail.com>
2026-04-17 09:47:59 +0800

    fix: correct android_assets path in WebView

--- app/src/main/java/com/aitutor/app/MainActivity.java
+++ app/src/main/java/com/aitutor/app/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        webView.loadUrl("file:///android_asset/index.html");
+        webView.loadUrl("file:///android_assets/index.html");
     }
 
     @Override
